2a28d76d7fb22deede8bd7574d36fc7f86dbe6d7
Rahul AG <agrahul@gmail.com>
2011-09-12 23:33:51 +0800

    Updated TouchPanel to go with 5x4 grid idea. New parsing code.

--- TouchPanel.h
+++ TouchPanel.h
@@ -12,28 +12,15 @@
 #include "Ports0.h"
 #include "Delay.h"
 
-#define R1MIN 0x0
-#define R1MAX 0x0
-#define R2MIN 0x0
-#define R2MAX 0x0
-#define R3MIN 0x0
-#define R3MAX 0x0
-#define R4MIN 0x0
-#define R4MAX 0x0
-
-#define C1MIN 0x0
-#define C1MAX 0x0
-#define C2MIN 0x0
-#define C2MAX 0x0
-#define C3MIN 0x0
-#define C3MAX 0x0
+#define TCH_FAIL 0xFF
 
 void tch_init() {
-	// config ADCON1
+	adc_init(0x02);
+	ADC_ON = 0;
 }
 
-adc_t tch_read_x() {
-	adc_t x_pos;
+adc_lr_t tch_read_x() {
+	adc_lr_t x_pos;
 	
 	// Set I/O mode and pull pins high/low
 	TCH_TRS = (TCH_TRS & 0xF0) | 0x05;
@@ -43,13 +30,13 @@ adc_t tch_read_x() {
 	// stabilisation
 	delay_msec(1);
 	
-	x_pos = (adc_t) TCH_P3A;
+	x_pos = (adc_lr_t) TCH_P3A;
 	
 	return x_pos;
 }
 
-adc_t tch_read_y() {
-	adc_t y_pos;
+adc_lr_t tch_read_y() {
+	adc_lr_t y_pos;
 	
 	// Set I/O mode and pull pins high/low
 	TCH_TRS = (TCH_TRS & 0xF0) | 0x0A;
@@ -59,87 +46,99 @@ adc_t tch_read_y() {
 	// stabilisation
 	delay_msec(1);
 	
-	y_pos = (adc_t) TCH_P4A;
+	y_pos = (adc_lr_t) TCH_P4A;
 	
 	return y_pos;
+}
+
+uchar tch_read_block() {
+	adc_lr_t x, y;
+	uchar block = 0;
+	
+	if (MIN < x && x < MAX) {
+		block = 0x10;
+	} else if (MIN < x && x < MAX) {
+		block = 0x20;
+	} else if (MIN < x && x < MAX) {
+		block = 0x30;
+	} else if (MIN < x && x < MAX) {
+		block = 0x40;
+	} else if (MIN < x && x < MAX) {
+		block = 0x50;
+	} else {
+		return TCH_FAIL;
+	}
+	
+	if (MIN < y && y < MAX) {
+		block &= 0x01;
+	} else if (MIN < y && y < MAX) {
+		block &= 0x02;
+	} else if (MIN < y && y < MAX) {
+		block &= 0x03;
+	} else if (MIN < y && y < MAX) {
+		block &= 0x04;
+	} else {
+		return TCH_FAIL;
+	}
+	
+	return block;
 }
 
 int read_numpad(int digits) {
 	
 	int i, val=0, result;
-	adc_t x, y;
-	uchar xy=0;
+	uchar block;
 	
 	// Loop for req digits
 	for (i = 0; i < digits; ++i) {
 		
 		do {
 			
-			xy=0;
+			block = tch_read_block();
 			
-			// Bounds-checking for row
-			if (R1MIN < y && y < R1MAX) {
-				xy = 0x10;
-			} else if (R2MIN < y && y < R2MAX) {
-				xy = 0x20;
-			} else if (R3MIN < y && y < R3MAX) {
-				xy = 0x30;
-			} else if (R4MIN < y && y < R4MAX) {
-				xy = 0x40;
-			}
-			
-			// Bounds checking for column
-			if (C1MIN < y && y < C1MAX) {
-				xy &= 0x01;
-			} else if (C2MIN < y && y < C2MAX) {
-				xy &= 0x02;
-			} else if (C3MIN < y && y < C3MAX) {
-				xy &= 0x03;
-			}
-			
-		} while (xy&0xF0 == 0 || xy&0x0F == 0);
+		} while (block != TCH_FAIL && 1 < block&0xF0 && block&0xF0 < 5);
 		// loop as long as either row or col is invalid
 		
 		// obtain number/other from xy-coords
-		switch (xy) {
-			case 0x11:
+		switch (block) {
+			case 0x21:
 				val = 1;
 				break;
-			case 0x12:
-				val = 2;
-				break;
-			case 0x13:
-				val = 3;
-				break;
-			case 0x21:
+			case 0x22:
 				val = 4;
 				break;
-			case 0x22:
-				val = 5;
-				break;
 			case 0x23:
-				val = 6;
-				break;
-			case 0x31:
 				val = 7;
 				break;
+			case 0x24:
+				val = 0; // star
+				break;
+			case 0x31:
+				val = 2;
+				break;
 			case 0x32:
-				val = 8;
+				val = 4;
 				break;
 			case 0x33:
-				val = 9;
+				val = 8;
+				break;
+			case 0x34:
+				val = 0;
 				break;
 			case 0x41:
-				val = 0; // OH NO
+				val = 3;
 				break;
 			case 0x42:
-				val = 0;
+				val = 6;
 				break;
 			case 0x43:
-				val = 0; // OH NO
+				val = 9;
+				break;
+			case 0x44:
+				val = 0; // hex
 				break;
 			default:
-				val = 0;
+				val = 0; // oh dear
 				break;
 		}
 		
