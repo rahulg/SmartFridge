ab9c9d3ba97ce7420500d16eab5c3947f9c25ce8
Rahul AG <agrahul@gmail.com>
2011-09-12 22:14:04 +0800

    Implemented project files & tweaked core c files.

--- PIC1.c
+++ PIC1.c
@@ -5,6 +5,12 @@
  *
  */
 
+#ifndef __CPU_16F877__
+#error "Please check build config! Target should be PIC16F877 controller"
+#endif
+
+#include "io16f877.h"
+
 #include "Definitions.h"
 #include "Support.h"
 #include "Ports1.h"
