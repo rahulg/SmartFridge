3fca8309dad61e0867d49fb5e15b801aa9fa7c20
Rahul AG <agrahul@gmail.com>
2011-10-03 01:48:52 +0800

    Implemented UART sync
    UART: Added enum for connection state
    PIC0: Implemented ISR to handle UART communication

--- PIC0.c
+++ PIC0.c
@@ -16,7 +16,54 @@
 #include "Delay.h"
 #include "Ports0.h"
 #include "LCD.h"
+#include "UART.h"
 #include "UserInterface.h"
+
+volatile syncstate sync_state = SYNC_IDLE;
+volatile uchar inbuf;
+
+/*
+ * Interrupt Service Routine
+ */
+#pragma vector=0x04
+__interrupt void isr(void)
+{
+	if (RCIE == 1) {
+		if (sync_state == SYNC_IDLE) {
+			inbuf = RCREG;
+			if (inbuf == ENQ) {
+				TXREG = ACK;
+				sync_state = SYNC_WCMD;
+			} else {
+				TXREG = NAK;
+				sync_state = SYNC_IDLE;
+			}
+		} else if (sync_state == SYNC_WCMD) {
+			inbuf = RCREG;
+			if (inbuf == DC1) {
+				TXREG = 0x00; // TODO: load low state
+				sync_state = SYNC_WACK;
+			} else if (inbuf == CAN) {
+				TXREG = ACK;
+				sync_state = SYNC_IDLE;
+			} else {
+				TXREG = NAK;
+				sync_state = SYNC_IDLE;
+			}
+		} else if (sync_state == SYNC_WACK) {
+			inbuf = RCREG;
+			if (inbuf == ACK) {
+				TXREG = EOT;
+				sync_state = SYNC_IDLE;
+			} else {
+				TXREG = NAK;
+				sync_state = SYNC_IDLE
+			}
+		} else {
+			sync_state = SYNC_IDLE;
+		}
+	}
+}
 
 int main(void) {
 	
